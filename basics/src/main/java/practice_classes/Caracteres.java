621eab30ebea0d8f038e499d94d312ccdb5fe631
AngelKode <200angel@live.com.mx>
2024-08-07 22:13:24 -0600

    Character class started

---
+++ basics/src/main/java/practice_classes/Caracteres.java
@@ -0,0 +1,12 @@
+package practice_classes;
+
+public class Caracteres {
+    public static void main(String[] args) {
+
+        char myChar = '@';
+        char decimal = 64;
+        System.out.println(myChar);
+        System.out.println(decimal);
+
+    }
+}
