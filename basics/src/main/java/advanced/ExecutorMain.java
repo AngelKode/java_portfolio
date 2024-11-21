9810f162f625f39b8e3edde18ad13255c4cc4c28
AngelKode <200angel@live.com.mx>
2024-11-20 22:59:24 -0600

    Thread fixed learned

--- basics/src/main/java/advanced/ExecutorMain.java
+++ basics/src/main/java/advanced/ExecutorMain.java
@@ -6,51 +6,68 @@ import java.util.concurrent.*;
 public class ExecutorMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
 
-        ExecutorService threadExecutor = Executors.newSingleThreadExecutor();
+        Future<ArrayList<String>> queryResult;
+        Future<ArrayList<String>> queryResultSecond;
+        Future<ArrayList<String>> queryResultNew;
+        try (ExecutorService threadExecutor = Executors.newFixedThreadPool(2)) {
 
-        //Task to be executed
-        //Runnable dont return value
-        /*
-        Runnable myThread = () -> {
-            System.out.println("Consulting DB from " + Thread.currentThread().getName());
+            Callable<ArrayList<String>> callableTask = () -> {
+                System.out.println("Searching clients...");
+                ArrayList<String> clients = new ArrayList<>(5);
+                clients.add("Juan");
+                clients.add("Juan1");
+                clients.add("Juan2");
+                clients.add("Juan3");
+                clients.add("Juan4");
+                TimeUnit.SECONDS.sleep(3);
 
-            try {
-                TimeUnit.SECONDS.sleep(6);
-            } catch (InterruptedException e) {
-                //If something happens
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
+                return clients;
+            };
+
+            Callable<ArrayList<String>> callableTask2 = () -> {
+                System.out.println("Searching clients...");
+                ArrayList<String> clients = new ArrayList<>(5);
+                clients.add("Juan4");
+                clients.add("Juan3");
+                clients.add("Juan2");
+                clients.add("Juan1");
+                clients.add("Juan");
+                TimeUnit.SECONDS.sleep(2);
+
+                return clients;
+            };
+
+            queryResult = threadExecutor.submit(callableTask);
+            queryResultSecond = threadExecutor.submit(callableTask);
+            queryResultNew = threadExecutor.submit(callableTask2);
+
+            threadExecutor.shutdown();
+
+            while (!(queryResult.isDone() && queryResultSecond.isDone() && queryResultNew.isDone())){
+                System.out.println(
+                        String.format("Thread Status:\n queryResult: %s\n queryResultSecond : %s \n queryResultNew: %s",
+                                queryResult.state().toString(),
+                                queryResultSecond.state().toString(),
+                                queryResultNew.state().toString()
+                ));
+                TimeUnit.MILLISECONDS.sleep(1500);
             }
-            System.out.println("Consulting ending from " + Thread.currentThread().getName());
-        };
 
-        Future<?> executorResult = threadExecutor.submit(myThread);
-        threadExecutor.shutdown();
-        System.out.println(executorResult.get(10,TimeUnit.SECONDS));
-        System.out.println(executorResult.isDone());
-*/
-        //Task to be executed
-        //Callable return value
+            //Print results
+            System.out.println("Clients for thread 1: ");
+            for(String client : queryResult.get(5,TimeUnit.SECONDS)){
+                System.out.print(client + ",");
+            }
 
-        Callable<ArrayList<String>> callableTask = () -> {
-            System.out.println("Searching clients...");
-            ArrayList<String> clients = new ArrayList<>(5);
-            clients.add("Juan");
-            clients.add("Juan1");
-            clients.add("Juan2");
-            clients.add("Juan3");
-            clients.add("Juan4");
-            TimeUnit.SECONDS.sleep(5);
+            System.out.println("\nClients for thread 2: ");
+            for(String client : queryResultSecond.get(5,TimeUnit.SECONDS)){
+                System.out.print(client + ",");
+            }
 
-            return clients;
-        };
-
-        Future<ArrayList<String>> queryResult = threadExecutor.submit(callableTask);
-        threadExecutor.shutdown();
-        for (String client : queryResult.get(10,TimeUnit.SECONDS)) {
-            System.out.println("Client " + client);
+            System.out.println("\nClients for thread 3: ");
+            for(String client : queryResultNew.get(5,TimeUnit.SECONDS)){
+                System.out.print(client + ",");
+            }
         }
-
-
     }
 }
